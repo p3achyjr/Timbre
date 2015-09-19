c149261e76b203a6b5f9d1690ff6d38578888799
Anatol Liu <axl@andrew.cmu.edu>
2015-09-19 13:20:57 -0400

    Initial Commit

---
+++ Timbre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Timbre
+//
+//  Created by Anatol Liu on 9/19/15.
+//  Copyright (c) 2015 AnatolSusan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
